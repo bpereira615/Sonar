d3ff9c066e40596c6e4b307ea932a39fc60062da
bpereira615 <bpereira615@gmail.com>
2016-04-16 01:49:24 -0400

    Began part 2, receiving digital system

--- sonar_driver.m
+++ sonar_driver.m
@@ -1,1 +1,16 @@
+Fs = 100;
+
+%% Part 1 - Sonar Detection
 distance = sonar_detection('ActiveSonar.mat');
+
+
+%% Part 2 - Digital Message Reception
+
+load('ReceivedSignal.mat');
+
+%h, matchted filter for logical 1 (negative for logical 0)
+h = 0:1/Fs:0.5;
+%flip and shift ramp function to get response
+h = [zeros(size(h)), fliplr(h)];
+
+msg=receiver(ReceivedSignal, h);

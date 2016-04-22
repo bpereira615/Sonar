cc76d1a00e9b848539a6a92fd171ec82a3ef5f2a
Benjamin Hoertnagl-Pereira <bpereira615@gmail.com>
2016-04-22 09:34:11 -0400

    Digital receiver updated

--- receiver.m
+++ receiver.m
@@ -2,26 +2,34 @@ function [ msg ] = receiver( x, h )
 %receiver matched filter technique, decoding message
 %   Detailed explanation goes here
 
-logic1 = h;
-logic0 = -1.*h;
 
-y1 = conv(x, logic1);
-y0 = conv(x, logic0);
+y = conv(x, h);
 
-msg = zeros(size(x));
-
-%first 51 indices are just zero, start in correct position
-curr = 52;
+t = 50;
 i = 1;
-while curr < length(y1)
-    abs(y1)
-    abs(y0)
-    if (abs(y1) > abs(y0))
+msg = zeros(1, length(y)/50 - 1);
+
+while t < length(x)
+    if x(t) > 0
         msg(i) = 1;
+    else
+        msg(i) = 0;
     end
     
-    curr = curr + (0.5 * 100);
+    t = t + 50;
+    i = i + 1;
 end
+
+%may need to check the very last bit outside of this
+    if x(end) > 0
+        msg(end) = 1;
+    else
+        msg(end) = 0;
+    end
+
+    
+    
+    
 
 end
 
---
+++ receiver_fft.m
@@ -0,0 +1,7 @@
+function [ ] = receiver_fft( x, h )
+%receiver_fft frequency domain matched filter technique, decoding message
+%   Detailed explanation goes here
+
+
+end
+
